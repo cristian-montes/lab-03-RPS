558469efe8e6f8e1211df3c92154376456b405a4
Cristian Montes <cmontesmujica@gmail.com>
2021-07-01 14:55:56 -0700

    utils.js added and funRandom fuction added

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ const winsSpan = document.getElementById('total-wins');
 const losesSpan = document.getElementById('loses-wins');
 const drawsSpan = document.getElementById('draws-wins');
 
+// let randomChoice = Math.ceil(Math.random()*3)
+// console.log(randomChoice);
 
   // INITIALIZE STATE
 let wins = 0;
@@ -21,7 +23,26 @@ let draws = 0;
 
 
 
+
+// funRandomNum function
+function funRandomNum(randomNum){
+    randomNum = Math.ceil(Math.random() * 3);
+    if (randomNum === 1){
+        return 'rock';
+    } else if (randomNum === 2) {
+        return 'paper';
+    } else {
+        return 'scissors';
+    }
+}
+
+
+
+
+
+
   // RSP BUTTON EVENT LISTENER
 rspBtn.addEventListener('click', ()=> {
 
 });
+
