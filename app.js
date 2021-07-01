6ccdef0464f5d95240ce5d35af3c76eb4e445149
Cristian Montes <cmontesmujica@gmail.com>
2021-07-01 14:30:10 -0700

    DOM elements in app.js and empty event listener added

--- app.js
+++ app.js
@@ -6,3 +6,22 @@
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
+  // GET DOM ELEMENTS
+const rspBtn = document.getElementById('rsp-btn');
+const winsSpan = document.getElementById('total-wins');
+const losesSpan = document.getElementById('loses-wins');
+const drawsSpan = document.getElementById('draws-wins');
+
+
+  // INITIALIZE STATE
+let wins = 0;
+let loses = 0;
+let draws = 0;
+
+
+
+  // RSP BUTTON EVENT LISTENER
+rspBtn.addEventListener('click', ()=> {
+
+});
